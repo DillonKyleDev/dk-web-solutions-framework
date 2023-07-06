cc1b8c4119ecda0aaf6bd237953a8a848ab7231e
DillonKyleDev <dillonkyle95@protonmail.com>
2023-07-05 20:50:21 -0700

    sdflihtse

--- src/modules/two-column.module/fields.js
+++ src/modules/two-column.module/fields.js
@@ -6,9 +6,17 @@ const setFieldParams = (field, params) => {
 
 module.exports = (fieldOptions) => {
   const styles = JSON.parse(fs.readFileSync('../../json/styles.json'));
+  const spacing = JSON.parse(fs.readFileSync('../../json/spacing.json'));
   const column = JSON.parse(fs.readFileSync('../../json/column.json'));
 
-  let fields = setFieldParams(column, {});
+  let fields = [
+    setFieldParams(spacing, {
+      name: 'module_spdacing',
+      label: 'Module Spacing',
+      default: 'medium',
+    })
 
-  return [...column, styles];
+  ];
+
+  return [column];
 };
